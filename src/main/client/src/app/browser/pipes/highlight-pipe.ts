b84c1d77218fe80386f57fd47085402b1aa20972
Michael Gargano <Michael.Gargano@jax.org>
2018-02-23 15:49:43 -0500

    Reworking the highlight

--- src/main/client/src/app/browser/pipes/highlight-pipe.ts
+++ src/main/client/src/app/browser/pipes/highlight-pipe.ts
@@ -6,21 +6,7 @@ export class HighlightPipe implements PipeTransform {
     let outHtml: string = "";
     if (result && query) {
       const regex = new RegExp(query, 'gi');
-      result.name = result.name.replace(regex, (match) => `<span class="search-highlight">${match}</span>`);
-      outHtml = `<span class="result">
-                   <span class="id">${result.id}</span> 
-                   <span class="name">${result.name}</span>
-                 </span>
-                `;
-      if(result.count && result.count != 0){
-       outHtml = `<span class="result">
-                   <span class="id">${result.id}</span>  
-                   <span class="name">${result.name}</span>
-                  </span>
-                  <span matTooltip="# of Descendants" matTooltipPosition="left" class="badge search-badge">${result.count}</span>
-                `;
-      }
-      return outHtml;
+      return  result.name.replace(regex, (match) => `<span class="search-highlight">${match}</span>`);
     }
   }
 }
