4eb7197865815ee30703d72d3b9c2da462afa182
Michael Gargano <Michael.Gargano@jax.org>
2018-02-23 12:18:31 -0500

    de-emphasizing the identifiers

--- src/main/client/src/app/browser/pipes/highlight-pipe.ts
+++ src/main/client/src/app/browser/pipes/highlight-pipe.ts
@@ -8,17 +8,18 @@ export class HighlightPipe implements PipeTransform {
       const regex = new RegExp(query, 'gi');
       result.name = result.name.replace(regex, (match) => `<span class="search-highlight">${match}</span>`);
       outHtml = `<span class="result">
-                 ${result.id} ${result.name}
+                   <span class="id">${result.id}</span> 
+                   <span class="name">${result.name}</span>
                  </span>
                 `;
       if(result.count && result.count != 0){
        outHtml = `<span class="result">
-                 ${result.id} ${result.name}
-                 </span>
-                 <span matTooltip="# of Descendants" matTooltipPosition="left" class="badge search-badge">${result.count}</span>
+                   <span class="id">${result.id}</span>  
+                   <span class="name">${result.name}</span>
+                  </span>
+                  <span matTooltip="# of Descendants" matTooltipPosition="left" class="badge search-badge">${result.count}</span>
                 `;
       }
-
       return outHtml;
     }
   }
