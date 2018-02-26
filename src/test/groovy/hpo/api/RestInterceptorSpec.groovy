4ddf2a21bd9fd7804b07adfacd656bd3a900fa9e
Michael Gargano <Michael.Gargano@jax.org>
2018-02-26 10:43:15 -0500

    Testing for rest interceptor

--- src/test/groovy/hpo/api/RestInterceptorSpec.groovy
+++ src/test/groovy/hpo/api/RestInterceptorSpec.groovy
@@ -18,5 +18,31 @@ class RestInterceptorSpec extends Specification implements InterceptorUnitTest<R
 
         then:"The interceptor does match"
             interceptor.doesMatch()
+
+    }
+
+    void "Test rest interceptor renders headers"(){
+      given:
+      request.method = "GET"
+
+      and:
+      interceptor.before()
+
+      expect:
+      response.getHeaderNames().containsAll(["X-Frame-Options", "X-Content-Type-Options","X-XSS-Protection",
+                                    "Content-Security-Policy"])
+    }
+    void "Test rest interceptor renders headers and values"(){
+      given:
+      request.method = "GET"
+
+      and:
+      interceptor.before()
+
+      expect:
+      response.getHeader("X-Frame-Options") == "DENY"
+      response.getHeader("X-Content-Type-Options") == "nosniff"
+      response.getHeader("X-XSS-Protection") == "1"
+      response.getHeader("Content-Security-Policy") == "script-src 'self' 'unsafe-eval'"
     }
 }
