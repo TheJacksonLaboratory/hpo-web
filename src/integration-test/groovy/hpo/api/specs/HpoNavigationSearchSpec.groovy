d675f5f1487676670e896673809019f54abd5324
Michael Gargano <Michael.Gargano@jax.org>
2018-03-19 13:10:52 -0400

    Functional tests, increasing size of search bar

---
+++ src/integration-test/groovy/hpo/api/specs/HpoNavigationSearchSpec.groovy
@@ -0,0 +1,33 @@
+package hpo.api.specs
+
+import geb.spock.GebReportingSpec
+import grails.testing.mixin.integration.Integration
+import grails.transaction.Rollback
+
+import geb.spock.*
+import hpo.api.pages.DownloadsPage
+
+@Integration
+@Rollback
+class HpoNavigationSearchSpec extends GebReportingSpec {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test navigation term search input" (){
+
+      given:
+      DownloadsPage downloadsPage = browser.to(DownloadsPage)
+
+      when:
+      downloadsPage.navSearchBar.value('limbs')
+
+      then:
+      downloadsPage.navSearchResults[0].text() == "Abnormality of limbs"
+    }
+
+}
+
