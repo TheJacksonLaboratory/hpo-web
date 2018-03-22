12742f65e8faa7087c0edaad1dcd6b7ddeb7b181
Michael Gargano <Michael.Gargano@jax.org>
2018-03-22 16:10:21 -0400

    Updating routing for tools

--- src/main/client/src/app/static/tools/tools-routing.module.ts
+++ src/main/client/src/app/static/tools/tools-routing.module.ts
@@ -11,17 +11,24 @@ import { GenomiserComponent } from './genomiser/genomiser.component';
 import { OtherComponent } from './other/other.component';
 import { ClinicalAnnotationComponent } from './clinicalannotation/clinicalanno.component';
 import { InternalToolsComponent } from './internaltools/internaltools.component';
+import {ToolsComponent} from "./tools.component";
 
 const toolsRoutes: Routes = [
-  { path: '', component: OverviewComponent},
-  { path: 'phenomizer', component: PhenomizerComponent},
-  { path: 'phenogramviz', component: PhenogramVizComponent},
-  { path: 'exomiser', component: ExomiserComponent},
-  { path: 'genomiser', component: GenomiserComponent},
-  { path: 'clinical-annotation', component: ClinicalAnnotationComponent},
-  { path: 'internal', component: InternalToolsComponent},
-  { path: 'external', component: ExternalToolsComponent},
-  { path: 'other', component: OtherComponent}
+  { path: 'tools',
+    component: ToolsComponent,
+    children: [
+      { path: 'tools', redirectTo:'overview', pathMatch: 'prefix'},
+      { path: 'overview',  component: OverviewComponent},
+      { path: 'phenomizer', component: PhenomizerComponent},
+      { path: 'phenogramviz', component: PhenogramVizComponent},
+      { path: 'exomiser', component: ExomiserComponent},
+      { path: 'genomiser', component: GenomiserComponent},
+      { path: 'clinical-annotation', component: ClinicalAnnotationComponent},
+      { path: 'internal', component: InternalToolsComponent},
+      { path: 'external', component: ExternalToolsComponent},
+      { path: 'other', component: OtherComponent}
+    ]
+  }
 ];
 
 export const toolsRouting = RouterModule.forChild(toolsRoutes);
