1433f6f5b9c7d6d7ee0c444fc6dfd032370fa7d1
Michael Gargano <Michael.Gargano@jax.org>
2018-05-10 11:01:54 -0400

    Downloads seperated into two pages

---
+++ src/main/client/src/app/static/downloads/ontology/ontology.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ontology',
+  templateUrl: './ontology.component.html',
+  styleUrls: ['./ontology.component.css']
+})
+export class OntologyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
