90fb8e80d624595f8d1f963e441e1258a7b16336
Francisco Castellanos <francisco.castellanos@jax.org>
2018-05-11 17:10:27 -0400

    HPO-107 functional test

---
+++ src/integration-test/groovy/hpo/api/pages/GeneDetailsPage.groovy
@@ -0,0 +1,28 @@
+package hpo.api.pages
+
+import geb.Page
+
+class GeneDetailsPage extends Page{
+
+  static final String TITLE = 'HPO'
+
+  static url = "/app/browse/gene/29123"
+
+  static at = {
+    title == TITLE
+  }
+
+  static content = {
+
+    termTabElement(wait:true) {$"#mat-tab-label-0-1"}
+    termFilterElement(wait:true, required:false) { $("#termFilterInput")}
+    termPagingRangeLabelElement(wait:true, required: false) {$(".mat-paginator-range-label")}
+
+    diseaseTabElement(wait:true) {$"#mat-tab-label-0-2"}
+    diseaseFilterElement(wait:true, required:false) { $("#diseaseFilterInput")}
+    diseasePagingRangeLabelElement(wait:true, required: false) {$(".mat-paginator-range-label")}
+
+
+  }
+
+}
