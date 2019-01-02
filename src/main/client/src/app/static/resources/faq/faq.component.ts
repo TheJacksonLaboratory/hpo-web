7e5b29b4e83f7c62bb87b13372376f7de64da9ea
Michael Gargano <Michael.Gargano@jax.org>
2019-01-02 09:56:02 -0500

    faq scrolling by id

--- src/main/client/src/app/static/resources/faq/faq.component.ts
+++ src/main/client/src/app/static/resources/faq/faq.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-faq',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
-  constructor() {
 
-  }
+  openPanel: number;
+  panelHash =  {
+    1: 'hpo-relation',
+    2: 'content-change',
+    3: 'collaboration',
+    4: 'ontology',
+    5: 'annotation-use',
+    6: 'genes-phenotypes',
+    7: 'frequencies',
+    8: 'association-rules'
+  };
+
+  constructor(private route: ActivatedRoute, private scroller: ViewportScroller) {}
+
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.navigateToQuestion(+params['id']);
+    });
   }
 
+  /***
+   * Opens the designated question denoted
+   * by /:id & then scrolls to it.
+   * @param {number} q
+   */
+  navigateToQuestion(q: number) {
+    if (Object.keys(this.panelHash).includes(q.toString())) {
+      this.openPanel = q;
+      const element = document.getElementById(this.panelHash[q]);
+      element.scrollIntoView();
+    }
+  }
 }
