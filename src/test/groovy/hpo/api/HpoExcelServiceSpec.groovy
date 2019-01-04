e5bf5434044a50897f4f0a532672df90d664139b
Michael Gargano <Michael.Gargano@jax.org>
2019-01-04 13:15:39 -0500

    adding in empty tests

---
+++ src/test/groovy/hpo/api/HpoExcelServiceSpec.groovy
@@ -0,0 +1,18 @@
+package hpo.api
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class HpoExcelServiceSpec extends Specification implements ServiceUnitTest<HpoExcelService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
