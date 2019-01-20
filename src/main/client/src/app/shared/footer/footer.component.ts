b4f5aa8c5b22468e1d91f75257e8be6bd3e90cce
Michael Gargano <Michael.Gargano@jax.org>
2019-01-19 21:42:30 -0500

    string interpolation fix

--- src/main/client/src/app/shared/footer/footer.component.ts
+++ src/main/client/src/app/shared/footer/footer.component.ts
@@ -8,7 +8,7 @@ import {environment} from '../../../environments/environment';
 })
 export class FooterComponent implements OnInit {
   title: string;
-  version = 'hpo-web@${environment.VERSION} - hpo-obo@2018-12-21';
+  version = `hpo-web@${environment.VERSION} - hpo-obo@2018-12-21`;
 
   constructor() {
     this.title = 'Human Phenotype Ontology';
