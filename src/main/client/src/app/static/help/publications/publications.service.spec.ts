68222eba56473bed399d447b46db2d25495a5eb2
Michael Gargano <Michael.Gargano@jax.org>
2019-04-03 15:11:16 -0400

    fixing up for tests

--- src/main/client/src/app/static/help/publications/publications.service.spec.ts
+++ src/main/client/src/app/static/help/publications/publications.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PublicationsService } from './publications.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatTableModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 describe('PublicationsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule],
+    providers: [PublicationsService]}));
 
   it('should be created', () => {
     const service: PublicationsService = TestBed.get(PublicationsService);
