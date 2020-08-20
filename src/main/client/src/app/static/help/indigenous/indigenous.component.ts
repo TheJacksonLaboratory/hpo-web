179450c0e96f2a4ac53f35f2f456472f407ff6e1
Michael Gargano <Michael.Gargano@jax.org>
2020-08-20 13:00:42 -0400

    seperation of lyfe languages, reworking navbar contents

---
+++ src/main/client/src/app/static/help/indigenous/indigenous.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indigenous',
+  templateUrl: './indigenous.component.html',
+  styleUrls: ['./indigenous.component.css']
+})
+export class IndigenousComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
