bd2036a632227cf55f24c41c5f7e7be9f9fb1877
iimpulse <mgdlxd@gmail.com>
2020-10-14 11:34:41 -0400

    Updating data, bumping versions

--- src/main/client/src/app/shared/footer/footer.component.ts
+++ src/main/client/src/app/shared/footer/footer.component.ts
@@ -8,7 +8,7 @@ import {environment} from '../../../environments/environment';
 })
 export class FooterComponent implements OnInit {
   title: string;
-  version = `hpo-web@${environment.VERSION} - hpo-obo@2020-08-11`;
+  version = `hpo-web@${environment.VERSION} - hpo-obo@2020-10-12`;
 
   constructor() {
     this.title = 'Human Phenotype Ontology';
