47295ec98993dc26d2d6de8763f6b49827534550
iimpulse <michael.gargano@jax.org>
2021-12-21 10:09:53 -0700

    purging the god forsaken log4j 2.11

--- src/main/client/src/app/shared/footer/footer.component.ts
+++ src/main/client/src/app/shared/footer/footer.component.ts
@@ -8,7 +8,7 @@ import {environment} from '../../../environments/environment';
 })
 export class FooterComponent implements OnInit {
   title: string;
-  version = `hpo-web@${environment.VERSION} - hpo-obo@2021-08-02`;
+  version = `hpo-web@${environment.VERSION} - hpo-obo@2021-10-10`;
 
   constructor() {
     this.title = 'Human Phenotype Ontology';
