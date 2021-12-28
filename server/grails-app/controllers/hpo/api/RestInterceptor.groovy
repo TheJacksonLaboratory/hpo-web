64682e090f236dab449a240585fd2eeac5903f2c
iimpulse <michael.gargano@jax.org>
2021-12-27 23:08:48 -0700

    reving to grails 5, new node integration

---
+++ server/grails-app/controllers/hpo/api/RestInterceptor.groovy
@@ -0,0 +1,24 @@
+package hpo.api
+
+class RestInterceptor {
+
+    RestInterceptor(){
+      matchAll()
+    }
+
+    boolean before() {
+      if(!System.properties['grails.console']){
+        header( "X-Frame-Options", "DENY" )
+        header( "X-Content-Type-Options", "nosniff" )
+        header( "X-XSS-Protection", "1" )
+        header( "Content-Security-Policy", "script-src 'self' 'sha256-uGJV1INRCzRQ65HtahUNomtGV0G2E/dzVWsvQpazKHw=' https://www.googletagmanager.com/gtag/js https://www.google-analytics.com/analytics.js")
+      }
+      true
+    }
+
+    boolean after() { true }
+
+    void afterView() {
+        // no-op
+    }
+}
---
+++ server/src/integration-test/groovy/GebConfig.groovy
@@ -0,0 +1,30 @@
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+
+environments {
+
+  // run via “./gradlew -Dgeb.env=chrome iT”
+  chrome {
+    driver = { new ChromeDriver() }
+  }
+
+  // run via “./gradlew -Dgeb.env=chromeHeadless iT”
+  chromeHeadless {
+    driver = {
+      ChromeOptions o = new ChromeOptions()
+      o.addArguments('headless')
+      new ChromeDriver(o)
+    }
+  }
+
+}
+
+waiting {
+  timeout = 30
+  retryInterval = 1.0
+}
+
+atCheckWaiting = true
+cacheDriverPerThread = true
+
+reportsDir = System.getProperty('reportsDir','build/reports/geb')
