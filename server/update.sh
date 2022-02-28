6e6535950e1396f7546969bf8fb604a713b5eaef
iimpulse <michael.gargano@jax.org>
2022-02-28 11:18:01 -0700

    updating ontology and scripts

---
+++ server/update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Exit if command fails
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+# OMIM 2 GENE
+if [ -f src/main/resources/mim2gene_medgen.txt ]; then
+	mv src/main/resources/mim2gene_medgen.txt src/main/resources/mim2gene_medgen.txt.old
+fi
+wget -O src/main/resources/mim2gene_medgen.txt ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen
+
+# HPO OBO
+if [ -f src/main/resources/hp_mostrecent.obo ]; then
+	mv src/main/resources/hp_mostrecent.obo src/main/resources/hp_mostrecent.obo.old
+fi
+wget -O src/main/resources/hp_mostrecent.obo http://purl.obolibrary.org/obo/hp.obo
+
+# MAXO OBO
+if [ -f src/main/resources/maxo_mostrecent.obo ]; then
+	mv src/main/resources/maxo_mostrecent.obo src/main/resources/maxo_mostrecent.obo.old
+fi
+wget -O src/main/resources/maxo_mostrecent.obo https://raw.githubusercontent.com/monarch-initiative/MAxO/master/maxo-base.obo
+
+
+# ORPHANET 2 GENE
+if [ -f src/main/resources/orphanet_disease2gene.xml ]; then
+	mv src/main/resources/orphanet_disease2gene.xml src/main/resources/orphanet_disease2gene.xml.old
+fi
+wget -O src/main/resources/orphanet_disease2gene.xml http://www.orphadata.org/data/xml/en_product6.xml
+
+# GENE INFO 
+if [ -f src/main/resources/Homo_sapiens.gene_info.gz ]; then
+        mv src/main/resources/Homo_sapiens.gene_info.gz src/main/resources/Homo_sapiens.gene_info.gz.old
+fi
+wget -O src/main/resources/Homo_sapiens.gene_info.gz ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz
+
+# PHENOTYPE HPOA
+if [ -f src/main/resources/phenotype.hpoa ]; then
+        mv src/main/resources/phenotype.hpoa src/main/resources/phenotype.hpoa.old
+fi
+wget -O src/main/resources/phenotype.hpoa http://purl.obolibrary.org/obo/hp/hpoa/phenotype.hpoa
+
+# EXIT
+exit 0;
