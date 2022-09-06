78ab165bb7ca9784350a14349e011ae47f8ae2f9
Peter Robinson <peter.robinson@jax.org>
2022-09-06 08:06:10 -0400

    new static pages for phenopackets

---
+++ src/main/client/src/app/static/help/phenopacket/phenopacket.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-phenopacket',
+  templateUrl: './phenopacket.component.html',
+  styleUrls: ['./phenopacket.component.css']
+})
+export class PhenopacketComponent implements OnInit {
+  constructor() { }
+  ngOnInit() { }
+}
