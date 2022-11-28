b78229ae39cd7d3985b912a51e3943585f24b997
iimpulse <michael.gargano@jax.org>
2022-11-28 14:05:06 -0700

    fixing update script and search results

--- server/update.sh
+++ server/update.sh
@@ -7,23 +7,23 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # OMIM 2 GENE
-wget -P server/src/main/resources/ -N ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen
+wget -P src/main/resources/ -N ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/mim2gene_medgen
 
 # HPO OBO
-wget -P server/src/main/resources/ -N http://purl.obolibrary.org/obo/hp.json
+wget -P src/main/resources/ -N http://purl.obolibrary.org/obo/hp.json
 
 # MAXO OBO
-wget -P server/src/main/resources/ -N https://raw.githubusercontent.com/monarch-initiative/MAxO/master/maxo.json
+wget -P src/main/resources/ -N https://raw.githubusercontent.com/monarch-initiative/MAxO/master/maxo.json
 
 
 # ORPHANET 2 GENE
-wget -P server/src/main/resources/ -N http://www.orphadata.org/data/xml/en_product6.xml
+wget -P src/main/resources/ -N http://www.orphadata.org/data/xml/en_product6.xml
 
 # GENE INFO
-wget -P server/src/main/resources/ -N http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
+wget -P src/main/resources/ -N http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
 
 # PHENOTYPE HPOA
-wget -P server/src/main/resources/ -N https://github.com/obophenotype/human-phenotype-ontology/releases/latest/download/phenotype.hpoa
+wget -P src/main/resources/ -N https://github.com/obophenotype/human-phenotype-ontology/releases/latest/download/phenotype.hpoa
 
 # EXIT
 exit 0;
