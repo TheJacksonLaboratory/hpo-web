505dd0432b662f356fb429566fe04c57f1ff3625
iimpulse <Michael.Gargano@jax.org>
2023-03-21 11:11:18 -0600

    add mondo linkout on disease page, new term search result

--- client/src/app/static/about/about.component.ts
+++ client/src/app/static/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilityService } from "../../shared/utility/utility.service";
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public utilityService: UtilityService) { }
 
   ngOnInit(): void {
   }
