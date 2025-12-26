9622d4d81741ab31dd998e95c2b9d27e38263229
iimpulse <Michael.Gargano@jax.org>
2025-12-26 10:22:28 -0700

    remove todo comment

--- src/app/static/feedback/feedback.component.ts
+++ src/app/static/feedback/feedback.component.ts
@@ -7,7 +7,6 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./feedback.component.scss']
 })
 export class FeedbackComponent {
-  // Replace this URL with your actual Google Form embed URL
   private formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSfID5F6is5J7GSJUlFzIYZ7FOsXEebjLjLZwXhLdFPELVBVMQ/viewform?usp=dialog';
   safeFormUrl: SafeResourceUrl;
 
