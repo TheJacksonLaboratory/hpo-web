0a7453e1c761f1639e6e84bdd618ec56d1db1514
iimpulse <Michael.Gargano@jax.org>
2026-04-16 15:18:50 -0600

    hero banner, search form, semi responsive

--- src/app/home/search-section/search-section.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-search-section',
-  standalone: true,
-  templateUrl: './search-section.component.html',
-  styleUrl: './search-section.component.scss',
-})
-export class SearchSectionComponent {}
