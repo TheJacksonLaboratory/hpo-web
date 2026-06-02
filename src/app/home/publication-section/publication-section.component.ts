b1c7f889974434283eff768b3e3d783fa6b342f1
iimpulse <Michael.Gargano@jax.org>
2026-06-02 13:50:26 -0600

    first pass on publication impl

--- src/app/home/publication-section/publication-section.component.ts
+++ src/app/home/publication-section/publication-section.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
+import { PublicationsService } from '../../static/resources/publications/publications.service';
+import { Publication } from '../../browser/models/models';
 
 @Component({
   selector: 'app-publication-section',
   standalone: true,
+  imports: [RouterLink, Card, Button],
   templateUrl: './publication-section.component.html',
   styleUrl: './publication-section.component.scss',
 })
-export class PublicationSectionComponent {}
+export class PublicationSectionComponent implements OnInit {
+  mostRecentPublication: Publication | null = null;
+
+  constructor(private pubService: PublicationsService) {}
+
+  ngOnInit(): void {
+    this.pubService.getPublications().subscribe(publications => {
+      this.mostRecentPublication = publications[0] ?? null;
+    });
+  }
+}
