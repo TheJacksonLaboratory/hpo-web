ed7dbf7350f77cd2e26f73c6896de67b3bff88a9
iimpulse <Michael.Gargano@jax.org>
2026-06-10 11:35:27 -0600

    lint fix

--- src/app/home/trusted-by-section/trusted-by-section.component.ts
+++ src/app/home/trusted-by-section/trusted-by-section.component.ts
@@ -16,12 +16,12 @@ export class TrustedBySectionComponent {
   readonly logos: OrgLogo[] = [
     { src: 'assets/logos/jax_logo.png', alt: 'The Jackson Laboratory' },
     { src: 'assets/logos/bih-logo.svg', alt: 'Berlin Institute of Health' },
-    { src: 'assets/logos/shriners_childrens.svg', alt: 'Shriners Childrens'},
+    { src: 'assets/logos/shriners_childrens.svg', alt: 'Shriners Childrens' },
     { src: 'assets/funding-images/DFG.jpg', alt: 'DFG' },
     { src: 'assets/funding-images/NIH.jpg', alt: 'NIH' },
     { src: 'assets/logos/monarch-logo.png', alt: 'Monarch Initiative' },
     { src: 'assets/logos/genomics_england_logo.png', alt: 'Genomics England' },
     { src: 'assets/funding-images/bmbf.png', alt: 'BMBF' },
-    { src: 'assets/logos/mayo-clinic-logo.svg', alt: 'Mayo Clinic'}
+    { src: 'assets/logos/mayo-clinic-logo.svg', alt: 'Mayo Clinic' }
   ];
 }
