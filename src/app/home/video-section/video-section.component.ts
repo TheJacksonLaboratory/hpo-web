0a6120eaff9810d596f4e9ff9b9d5b77780932f9
iimpulse <Michael.Gargano@jax.org>
2026-06-17 13:58:03 -0600

    video section impl

--- src/app/home/video-section/video-section.component.ts
+++ src/app/home/video-section/video-section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-section',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './video-section.component.html',
   styleUrl: './video-section.component.scss',
 })
-export class VideoSectionComponent {}
+export class VideoSectionComponent {
+  readonly videoUrl: SafeResourceUrl;
+
+  constructor(sanitizer: DomSanitizer) {
+    this.videoUrl = sanitizer.bypassSecurityTrustResourceUrl(
+      'https://www.youtube.com/embed/gjLVph13p6Y'
+    );
+  }
+}
